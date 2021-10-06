9716b00e09cc9e948c9c1ee6be41710d65382cc8
Henrik Bengtsson <hb@aroma-project.org>
2021-10-06 16:06:26 -0700

    ROBUSTNESS: Properly register native API

---
+++ src/000.init.c
@@ -0,0 +1,19 @@
+#include <R.h>
+#include <Rinternals.h>
+#include <stdlib.h> // for NULL
+#include <R_ext/Rdynload.h>
+
+void F77_SUB(scansnp)(int *n, double *maploc, double *het, double *keep, double *nbhd);
+void F77_SUB(t2maxo)(int *n, double *sx, int *ihet, int *iseg, double *ostat, int *nhet, double *rnij, double *rhij, double *delij);
+
+static const R_CallMethodDef CallEntries[] = {
+    {"scansnp", (DL_FUNC) &F77_SUB(scansnp), 5},
+    {"t2maxo",  (DL_FUNC) &F77_SUB(t2maxo),  9},
+    {NULL, NULL, 0}
+};
+
+void R_init_affxparser(DllInfo *dll)
+{
+    R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
+    R_useDynamicSymbols(dll, FALSE);
+}
